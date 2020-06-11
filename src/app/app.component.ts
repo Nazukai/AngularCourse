146efb2b1e98dbb0b1261dd12246ffcd578da7e9
Nicolas Thong <nicolas.thong@hotmail.com>
2020-06-11 09:46:25 +0200

    angularcourse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {COURSES} from '../db-data';
+import {Course} from './model/course';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Project';
+
+  courses = COURSES;
+
+  onCourseSelected(course:Course) {
+    console.log("Event", course);
+  }
+
 }
